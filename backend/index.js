785b72e708562ce567b5382ce4166aeefce019b3
yashwanth <yashwanth2601@gmail.com>
2024-11-13 18:05:20 +0530

    toto app

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use("/api/todos", todoRoutes);
 app.get('/', (req, res) => {
     res.send('Hello world')
   })
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const startServer=async()=>{
     try {
